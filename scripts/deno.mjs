036ded82aec15cbb26189b46a380a7603b3d1cee
Florian Klampfer <mail@qwtel.com>
2019-03-13 18:30:24 +0700

    Add deno target

---
+++ scripts/deno.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env node --experimental-modules --no-warnings
+
+import fs from 'fs';
+import { resolve, basename } from 'path';
+
+const { readFile, writeFile, readdir, stat } = fs.promises;
+
+async function* getFiles(dir) {
+  const subdirs = await readdir(dir);
+  for (const subdir of subdirs) {
+    const res = resolve(dir, subdir);
+    if ((await stat(res)).isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+const RE = /^(import|export)\s+(.*)\s+from\s+['"](.+)['"]\s*;*\s*$/gm;
+
+(async () => {
+    for await (const f of getFiles('./src')) {
+        let c = await readFile(f, 'utf8');
+        c = c.replace(RE, "$1 $2 from '$3.ts';");
+        await writeFile('./deno/' + basename(f), c, 'utf8');
+    }
+})();
